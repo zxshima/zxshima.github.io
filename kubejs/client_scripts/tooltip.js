2f5fdbc2bce4332e0fee917e863d3489dd387b3f
shima <hakase446@gmail.com>
2025-04-30 21:02:54 +0300

    0.7-pre1

---
+++ kubejs/client_scripts/tooltip.js
@@ -0,0 +1,4 @@
+ItemEvents.tooltip(event => {
+  event.add(['kubejs:lava_chicken'], '§6Spicy!')
+  event.add(['upgrade_aquatic:mulberry_pie'], '§dMessy!')
+  })
--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -7,6 +7,9 @@ ServerEvents.recipes(event => {
     event.remove({id: 'geodes:tuning/echo_tuning'})
     event.remove({id: 'upgrade_aquatic:trident'})
     event.remove({id: 'progressivebosses:trident'})
+    event.remove({id: 'ars_nouveau:novice_spell_book'})
+    event.remove({id: 'ars_nouveau:apprentice_spell_book_upgrade'})
+    event.remove({id: 'ars_nouveau:archmage_spell_book_upgrade'})
 
 
     event.remove({ output: 'alloyed:steel_fishing_rod' })
@@ -43,7 +46,15 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'decorative_blocks:rocky_dirt' })
     event.remove({ output: 'infinitybuttons:cherry_bookshelf_secret_button' })
     event.remove({ output: 'oreganized:bush_hammer' })
+    event.remove({ output: 'ad_astra:etrionic_blast_furnace' })
+    event.remove({ output: 'alloyed:steel_ingot' })
+    event.remove({ output: 'alloyed:steel_sheet' })
+    event.remove({ output: 'mowziesmobs:thatch_block' })
+    event.remove({ output: 'upgrade_aquatic:beachgrass_thatch' })
     event.remove({ input: 'minecraft:emerald_ore' })
+    event.remove({ input: 'minecraft:deepslate_emerald_ore' })
+    event.remove({ input: 'minecraft:lapis_ore' })
+    event.remove({ input: 'minecraft:deepslate_lapis_ore' })
 
 
     event.shaped('minecraft:map', [
@@ -91,5 +102,55 @@ ServerEvents.recipes(event => {
 		B: 'upgrade_aquatic:prismarine_rod'
 	}
 	);
+    event.shaped('ars_nouveau:novice_spell_book', [
+		'EBC',
+		'DAD',
+		'FGH'
+	], 	{
+		A: 'minecraft:book',
+		B: 'terramity:fairy_dust',
+		C: 'minecraft:lapis_lazuli',
+        D: 'create:brass_sheet',
+        E: 'ars_nouveau:sourceberry_bush',
+        F: 'caverns_and_chasms:silver_sword',
+        G: 'caverns_and_chasms:silver_pickaxe',
+        H: 'caverns_and_chasms:silver_axe'
+	}
+	);
+    event.shaped('ars_nouveau:apprentice_spell_book', [
+		'ECD',
+		'GAG',
+		'BFB'
+	], 	{
+		A: 'ars_nouveau:novice_spell_book',
+		B: 'minecraft:quartz_block',
+		C: 'cataclysm:abyss_eye',
+        D: 'terramity:prismatic_jewel',
+        E: 'terramity:profanum',
+        F: 'ars_nouveau:source_gem',
+        G: 'alloyed:bronze_sheet'
+	}
+	);
+    event.shaped('ars_nouveau:archmage_spell_book', [
+		'AHD',
+		'FCF',
+		'GBE'
+	], 	{
+		A: 'cataclysm:flame_eye',
+		B: 'minecraft:nether_star',
+		C: 'ars_nouveau:apprentice_spell_book',
+		D: 'quark:dragon_scale',
+		E: 'ars_nouveau:wilden_tribute',
+		F: 'terramity:cosmilite_ingot',
+		G: 'terramity:opaline_moonstone',
+		H: 'terramity:gundalfs_hat_helmet'
+	}
+	);
 	event.shapeless('kubejs:deer_cracker', ['2x minecraft:wheat', 'farmersdelight:rice']);
+    event.replaceInput(
+        { not: { output: 'ars_nouveau:starbuncle_charm' }, mod: 'ars_nouveau'},
+        'minecraft:gold_ingot',
+        'create:brass_ingot'
+    );
+
 })
